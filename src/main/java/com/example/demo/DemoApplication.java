079ce683420f6fd4a66671ef5934b9af579a7787
Ashish Sharma <asharma11380@altimetrik.com>
2023-11-20 16:52:43 +0530

    message updated

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		
-		System.out.println("application working");
+		System.out.println("application working.. in develop branch");
 	}
 
 }
